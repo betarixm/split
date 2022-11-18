de84060f6da8601cb1bd2c1be71418b63c610879
beta <beta.underalpha@gmail.com>
2022-11-18 20:43:26 +0900

    Resolve package issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@intlify/nuxt3', '@vueuse/nuxt'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@vueuse/nuxt'],
 
     intlify: {
         vueI18n: {
